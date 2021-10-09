5d7f5e11b4629525a0095b3c7601972e894cd72e
Lawsantos <laercio.legitimo@gmail.com>
2021-10-09 13:45:55 -0300

    Atualizando classe main Implementei o método imprimiCaracteristicas para o Ator e Diretor

--- Aula-02/src/main/java/Aplicacao.java
+++ Aula-02/src/main/java/Aplicacao.java
@@ -3,11 +3,17 @@ public class Aplicacao {
 
         Diretor diretor = new Diretor("Jackal", 50, 50, Genero.MASCULINO);
 
+        Ator ator = new Ator("Thais", 25,4,Genero.FEMININO);
+
         Filme filme = new Filme("filme de terror", "Pedra",1, 2021, 500, diretor);
 
         Filme selva = new Filme("Selva perigosa", "Filme com muito terror",60, 2019,5, diretor);
 
         selva.reproduzirFilmes();
+
+        diretor.imprimiCaracteristicas();
+
+        ator.imprimiCaracterisiticas();
     }
 
 }
